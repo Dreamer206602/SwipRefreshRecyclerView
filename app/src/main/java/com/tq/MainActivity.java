347e0f74de77837ed74b76c3ed95c7ac800c9c93
boobooL <boobooMX@163.com>
2016-03-31 10:16:20 +0800

    上拉刷新，下拉加载的基本完成

--- app/src/main/java/com/tq/MainActivity.java
+++ app/src/main/java/com/tq/MainActivity.java
@@ -9,15 +9,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
+import com.tq.adapter.RefreshAdapter;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.easydone.swiperefreshendless.EndlessRecyclerOnScrollListener;
 import cn.easydone.swiperefreshendless.HeaderViewRecyclerAdapter;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     @Bind(R.id.toolBar)
     Toolbar toolBar;
@@ -28,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.fab)
     FloatingActionButton fab;
 
-    private List<String>mList;
+    private List<String> mList;
     private HeaderViewRecyclerAdapter stringAdapter;
 
     @Override
@@ -41,25 +51,98 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,GridActivity.class));
+                startActivity(new Intent(MainActivity.this, GridActivity.class));
             }
         });
 
         swipeRefreshLayout.setColorSchemeColors(
                 Color.BLUE,
                 Color.GREEN,
-                Color.RED,Color.YELLOW);
+                Color.RED, Color.YELLOW);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        setDada();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
 
+        setDada();//初始化数据
+        RefreshAdapter refreshAdapter = new RefreshAdapter(mList, this);
+        stringAdapter = new HeaderViewRecyclerAdapter(refreshAdapter);
+        recyclerView.setAdapter(stringAdapter);
 
+        //加载footer布局
+        createLoadMoreView();
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+        recyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int currentPage) {
+                simulateLoadMoreData();
+            }
+        });
 
     }
 
-    private void setDada() {
+    private void simulateLoadMoreData() {
+        Observable.timer(2, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+                .map(new Func1<Long, Object>() {
+                    @Override
+                    public Object call(Long aLong) {
+                        loadMoreData();
+                        stringAdapter.notifyDataSetChanged();
+                        Toast.makeText(MainActivity.this, "Load Finished！", Toast.LENGTH_SHORT).show();
+                        return null;
+                    }
+                }).subscribe();
 
+    }
+
+    /**
+     * 下拉刷新，加载更多数据
+     */
+    private void loadMoreData() {
+
+        List<String> moreList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            moreList.add("加载更多的数据");
+        }
+        mList.addAll(moreList);
+
+    }
+
+    /**
+     * 加载footer布局
+     */
+    private void createLoadMoreView() {
+        View loadMoreView = LayoutInflater.from(MainActivity.this)
+                .inflate(R.layout.view_load_more, recyclerView, false);
+        stringAdapter.addFooterView(loadMoreView);
+    }
+
+    private void setDada() {
+        mList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            mList.add("第" + i + "个");
+        }
+
+    }
+
+    @Override
+    public void onRefresh() {
+        Observable.timer(2, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+                .map(new Func1<Long, Object>() {
+                    @Override
+                    public Object call(Long aLong) {
+                        fetchingNewData();
+                        swipeRefreshLayout.setRefreshing(false);
+                        stringAdapter.notifyDataSetChanged();
+                        Toast.makeText(MainActivity.this, "Refresh finished", Toast.LENGTH_SHORT).show();
+                        return null;
+                    }
+                }).subscribe();
+    }
+
+    private void fetchingNewData() {
+        mList.add(0, "下拉刷新出来的数据");
 
     }
 }
